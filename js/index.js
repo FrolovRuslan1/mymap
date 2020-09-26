e36a2b2b8cc3ddeeb2882bb505857d2d31d90713
Frolov Ruslan <gald.rus@yandex.ru>
2020-09-27 01:35:22 +0300

    scss structure + up buttons

---
+++ js/index.js
@@ -0,0 +1,13 @@
+(async () => {
+
+    let map = L.map('map');
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        zoomControl: true,
+        attributionControl: true
+    }).addTo(map);
+
+
+
+})()

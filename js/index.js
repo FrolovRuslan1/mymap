b25c9fc0d017fbe8952bcacb06459c527daa5898
Frolov Ruslan <gald.rus@yandex.ru>
2020-10-02 15:27:59 +0300

    close panel

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+import Vue from "vue/dist/vue";
+
+
+
 
 let map = L.map('map',{drawControl: true}).setView([55.7522, 37.6156], 13);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -7,13 +11,35 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 
 
+let map__leftPanel = new Vue({
+    el: document.getElementsByClassName('map__leftPanel')[0],
+    data: function () {
+        return {
+            isClose: true
+        }
+    }
+})
 
-
-new Vue({
+let map__closeLeftPanel = new Vue({
     el: document.getElementsByClassName('map__closeLeftPanel')[0],
     data: function () {
         return {
 
         }
+    },
+    methods: {
+        close: function (event) {
+            if (map__leftPanel.isClose === true) {
+                map__leftPanel.isClose = false;
+            }else {
+                map__leftPanel.isClose = true;
+            }
+
+        }
     }
 })
+
+
+
+
+
